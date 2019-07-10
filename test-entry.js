0627a69db8cc9ec209d56e962da88fddeaec63d3
zhuting03 <zhuting03@baidu.com>
2019-07-10 18:12:37 +0800

    fix: 切换部署分支

--- test-entry.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import TextAreaNoScroll from '../src/index';
-
-ReactDOM.render(<TextAreaNoScroll />, document.getElementById('app'));
