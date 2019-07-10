18ded94327c7df4b679d9518cddbdcb656b6949a
zhuting03 <zhuting03@baidu.com>
2019-07-10 18:29:28 +0800

    feat: 修改branch

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'production',
